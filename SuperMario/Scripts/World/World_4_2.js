403d1edfb56acd985c559803acbf91db30f18b1d
Leon <yang73137@126.com>
2014-12-17 21:25:14 +0800

    Add a new release.

--- SuperMario/Scripts/World/World_4_2.js
+++ SuperMario/Scripts/World/World_4_2.js
@@ -26,6 +26,18 @@ World_4_2 = ClassFactory.createClass(World, {
 
         ImageLoader.load(this, [Const.IMAGE_WORLD_4_3]);
         ScriptLoader.load(this, [Const.SCRIPT_WORLD_4_3]);
+
+        ImageLoader.load(this, [Const.IMAGE_WORLD_5_1]);
+        ScriptLoader.load(this, [Const.SCRIPT_WORLD_5_1]);
+
+        ImageLoader.load(this, [Const.IMAGE_WORLD_6_1]);
+        ScriptLoader.load(this, [Const.SCRIPT_WORLD_6_1]);
+
+        ImageLoader.load(this, [Const.IMAGE_WORLD_7_1]);
+        ScriptLoader.load(this, [Const.SCRIPT_WORLD_7_1]);
+
+        ImageLoader.load(this, [Const.IMAGE_WORLD_8_1]);
+        ScriptLoader.load(this, [Const.SCRIPT_WORLD_8_1]);
     },
     scroll: function () {
         if (!this.scrollable) {
@@ -415,13 +427,11 @@ World_4_2 = ClassFactory.createClass(World, {
         }
 
         var tube_7395_304 = new Block(7395, 304, 64, 96);
-        tube_7395_304.tip = false;
         tube_7395_304.attachCollidesUp(function(gameObject) {
             if (gameObject instanceof MarioBors && Input.isPressed(InputAction.DOWN)) {
-                if (!this.tip) {
-                    this.tip = true;
-                    alert("To be continued......");
-                }
+                this.state = WorldState.None;
+                var world = new World_5_1();
+                this.gameUI.gameUI.setWorld(world);
             }
         });
         tube_7395_304.addToGameUI(gameUI);
@@ -515,37 +525,31 @@ World_4_2 = ClassFactory.createClass(World, {
         block_10481_112.addToGameUI(gameUI);
 
         var tube_10545_304 = new Block(10545, 304, 64, 96);
-        tube_10545_304.tip = false;
         tube_10545_304.attachCollidesUp(function (gameObject) {
             if (gameObject instanceof MarioBors && Input.isPressed(InputAction.DOWN)) {
-                if (!this.tip) {
-                    this.tip = true;
-                    alert("To be continued......");
-                }
+                this.state = WorldState.None;
+                var world = new World_8_1();
+                this.gameUI.gameUI.setWorld(world);
             }
         });
         tube_10545_304.addToGameUI(gameUI);
 
         var tube_10673_304 = new Block(10673, 304, 64, 96);
-        tube_10673_304.tip = false;
         tube_10673_304.attachCollidesUp(function (gameObject) {
             if (gameObject instanceof MarioBors && Input.isPressed(InputAction.DOWN)) {
-                if (!this.tip) {
-                    this.tip = true;
-                    alert("To be continued......");
-                }
+                this.state = WorldState.None;
+                var world = new World_7_1();
+                this.gameUI.gameUI.setWorld(world);
             }
         });
         tube_10673_304.addToGameUI(gameUI);
 
         var tube_10801_304 = new Block(10801, 304, 64, 96);
-        tube_10801_304.tip = false;
         tube_10801_304.attachCollidesUp(function (gameObject) {
             if (gameObject instanceof MarioBors && Input.isPressed(InputAction.DOWN)) {
-                if (!this.tip) {
-                    this.tip = true;
-                    alert("To be continued......");
-                }
+                this.state = WorldState.None;
+                var world = new World_6_1();
+                this.gameUI.gameUI.setWorld(world);
             }
         });
         tube_10801_304.addToGameUI(gameUI);
